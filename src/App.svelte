750c8d3013cd56938c8961258142988f0743f58e
Isis☕Caffe <52106389+fum4@users.noreply.github.com>
2023-05-03 02:51:29 +0300

    Refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,102 @@
 <script lang="ts">
-  window.$(document).ready(function() {
+  import { onDestroy, onMount } from 'svelte';
+  import DOMPurify from 'isomorphic-dompurify';
+
+  const defaultContent = '<div style="text-align: left;"><br></div>';
+
+  function convertImageToBase64(img: HTMLImageElement) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    img.crossOrigin = 'Anonymous';
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    const dataURL = canvas.toDataURL('image/png');
+
+    img.src = dataURL.replace(/^data:image\/?[A-z]*;base64,/);
+  }
+
+  function convertAllImagesToBase64() {
+    const images = getEditorHTMLNode().querySelectorAll('img');
+
+    images.forEach((img) => {
+      if (img.src.startsWith('http')) {
+        convertImageToBase64(img);
+      }
+    });
+  }
+
+  function saveDraft() {
+    localStorage.setItem('draft', getEditorContent());
+  }
+
+  function clearDraft() {
+    localStorage.removeItem('draft');
+  }
+
+  function getDraft() {
+    return localStorage.getItem('draft');
+  }
+
+  function getEditorContent() {
+    return getEditorHTMLNode().innerHTML;
+  }
+
+  function getEditorHTMLNode(): HTMLDivElement {
+    return document.querySelector('[contenteditable="true"]');
+  }
+
+  function initializeEditor() {
     window.$('#summernote').summernote({
       height: 500,
       focus: true,
-      dialogsInBody: true,
       dialogsFade: true,
+      dialogsInBody: true,
       codemirror: {
         theme: 'monokai'
       },
       callbacks: {
         onInit: () => {
-          document.querySelector('[contenteditable="true"] > p').remove();
+          getEditorHTMLNode().innerHTML = '';
 
-          const draft = localStorage.getItem('draft');
-          const initialContent = '<p><br></p>';
+          const draft = getDraft();
 
-          if (draft && draft !== initialContent) {
-            window.$('#summernote').summernote('pasteHTML', draft);
-          } else {
-            window.$('#summernote').summernote('justifyLeft');
-          }
-        },
-        onChange: (contents) => {
-          if (contents) {
-            localStorage.setItem('draft', contents);
-          }
+          window.$('#summernote').summernote(
+            'pasteHTML',
+            draft || defaultContent
+          );
         }
       }
-    }).data("summernote");
+    });
+  }
+
+  function exportAsHTML(ev) {
+    ev.preventDefault();
+    // convertAllImagesToBase64();
+
+    const content = getEditorContent();
+
+    if (content) {
+      const sanitizedContent = DOMPurify.sanitize(content);
+
+      // TODO: Save export
+      console.log('Saving...', sanitizedContent);
+
+      getEditorHTMLNode().innerHTML = defaultContent;
+      getEditorHTMLNode().focus();
+      clearDraft();
+    }
+  }
+
+  onMount(() => {
+    initializeEditor();
+    window.onbeforeunload = saveDraft;
+  });
+
+  onDestroy(() => {
+    window.onbeforeunload = undefined;
   });
 </script>
 
@@ -37,10 +106,14 @@
     alt="Rinkt logo"
     class="logo"
   />
-  <form method="post">
+
+  <form method="post" on:submit={exportAsHTML}>
     <textarea id="summernote" name="editordata"></textarea>
+    <button
+      type="submit"
+      class="btn btn-primary ml-1"
+    >
+      OK
+    </button>
   </form>
 </main>
-
-<style>
-</style>
